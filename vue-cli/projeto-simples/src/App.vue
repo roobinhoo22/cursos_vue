c86e2c32b463b987ee5cba6ac67f32dd27a876d8
rodrigo.gomes <rodrigo.gomes@cejam.org.br>
2019-10-29 17:29:02 -0300

    projeto com plugins vue-cli

--- vue-cli/projeto-simples/src/App.vue
+++ vue-cli/projeto-simples/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      {{titulo}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+   data: function(){
+      return {
+         titulo: 'Olá esse é meu primeiro VUE-CLI'
+      }
+   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+       background-color: aqua;
+    }
 </style>
